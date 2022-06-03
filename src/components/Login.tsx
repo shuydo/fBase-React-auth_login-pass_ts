3d6b9d8f8ac799f388c277bc1b036ce6b29cb36e
Shuydo <shuydo12@gmail.com>
2022-06-03 17:34:08 +0300

    working

---
+++ src/components/Login.tsx
@@ -0,0 +1,40 @@
+// import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { Form } from './Form';
+import { setUser } from 'store/slices/userSlice';
+import { useAppDispatch } from 'hooks/redux-hooks';
+
+const Login = () => {
+  const dispatch = useAppDispatch();
+  // const dispatch = useDispatch();
+  const { push } = useHistory();
+
+  //ф-я аут-ии
+  const handleLogin = (email: string, password: string) => {
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, password)
+      .then(({ user }) => {
+        console.log(user);
+        dispatch(
+          setUser({
+            email: user.email,
+            id: user.uid,
+            // token: user.accessToken
+            token: user.refreshToken
+          })
+        );
+        push('/');
+      })
+      .catch(() => alert('Invalid user!'));
+
+    // signInWithEmailAndPassword(auth, email, password)
+    //   .then(console.log)
+    //   .catch(console.error);
+  };
+
+  return <Form title="sign in" handleClick={handleLogin} />;
+};
+
+export { Login };
